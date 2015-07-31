7e04299c0e981076ad5210f5b2cbabd0b9c355cf
Ten Mutunhire <tmutunhire@gmail.com>
2015-07-31 13:33:07 +0300

    Initial Commit

---
+++ SpaceMan/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  SpaceMan
+//
+//  Created by Ten Mutunhire on 7/31/15.
+//  Copyright (c) 2015 Lean Startup Space Launcher. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
